e2cc4372f8145dfea2f7bdf2f6a3abb42d7a66c8
Valdas K <elatier@gmail.com>
2012-02-05 15:13:16 +0000

    ...

--- Assignment 2/calculate_recall_precision.m
+++ Assignment 2/calculate_recall_precision.m
@@ -5,8 +5,9 @@ function rp_rates = calculate_recall_precision(conf_matrix)
 %
 %IN:  conf_matrix - a confusion matrix [NxN]
 %OUT: rp_rates - [N X 2] matrix with recall and precision values
-
-for i = 1:size(conf_matrix,1)
+dim = size(conf_matrix,1);
+rp_rates = zeros(dim,2);
+for i = 1:dim,
     rp_rates(i,1) = (conf_matrix(i,i) / sum(conf_matrix(i,:))) * 100; % Recall rate
     rp_rates(i,2) = (conf_matrix(i,i) / sum(conf_matrix(:,i))) * 100; % Precision rate
 end
