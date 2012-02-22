276a97bcddb516f9665ea5cfc493072b0cba9192
George Eracleous <basketballcy@gmail.com>
2012-02-22 23:35:12 +0000

    Small changes

--- Assignment 3/buildMultipleOutputNeuralNet.m
+++ Assignment 3/buildMultipleOutputNeuralNet.m
@@ -23,15 +23,3 @@ function neuralNet = buildMultipleOutputNeuralNet(P, T, generalizationMethod)
     network.trainParam.epochs = epochs;
     network = configure(network, P, T);
     neuralNet = trainbr(network, P, T);
-
-%   neuralNet = feedforwardnet(30,'trainscg');
-%   neuralNet = configure(neuralNet,P,T); 
-%   neuralNet.trainParam.epochs =100;
-%   neuralNet.trainParam.lr = 0.05;
-%   neuralNet.trainParam.lr_inc = 1.05;
-%   neuralNet.trainParam.goal = 0.01 ;
-%   neuralNet.trainParam.min_grad = 0.01;
-%   neuralNet.performFcn = 'msereg';
-%   neuralNet.performParam.ratio = 0.5;
-% 
-%   [neuralNet] = train(neuralNet, P, T);
