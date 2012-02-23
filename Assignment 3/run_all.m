6dce4e36fbdbd9c27ada720978ecade20adbedc9
George Eracleous <basketballcy@gmail.com>
2012-02-23 12:31:43 +0000

    Added axis labels and legend

--- Assignment 3/run_all.m
+++ Assignment 3/run_all.m
@@ -4,5 +4,12 @@ addpath(genpath(pwd));
 
 f_measures_multi = run_multi_output;
 f_measures_single = run_single_output;
+figure
 plot([1:10], f_measures_multi);
+hold on;
 plot([1:10], f_measures_single, 'r');
+title('F1 metric per fold');
+xlabel('Fold');
+ylabel('F1');
+hleg = legend('Multi output net','Single output nets');
+hold off;
