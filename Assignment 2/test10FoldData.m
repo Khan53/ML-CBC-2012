452e707f1eb31d4dfffa16730be8349f88ab5389
George Eracleous <basketballcy@gmail.com>
2012-03-15 15:39:04 +0000

    Some corrections

---
+++ Assignment 2/test10FoldData.m
@@ -0,0 +1,38 @@
+function [ f_measures_per_fold ] = test10FoldData( data_type )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+attribs = 1:45;
+
+[examples,targets] = loaddata( strcat(data_type,'data_students.txt'));
+
+
+%Start evaluating the learning algorithm using ten-fold cross validation.
+%predicted emotion labels (targets) from testSets 10x10, where each column
+%is a different test.
+predicted_targets_set = [];
+%given target labels for each test set
+actual_targets_set = [];
+trees_of_emotions = cell(1,6);
+f_measures_per_fold = cell(1, 10);
+
+for i = 0:9
+    [trainSet, testSet] = split_dataset(i, examples, targets);
+    %For each emotion, structure the target vector so that a '1' takes the place for an emotion
+    %and a '0' for the rest of the emotions. Then create the decision trees for each emotion.
+    for emotion = 1:6
+        theEmotions{emotion} = remap_targets(trainSet.targets,emotion);
+        trees_of_emotions{emotion} = decision_tree_learning(trainSet.examples,attribs,theEmotions{emotion});
+    end
+    predictions = testTrees(trees_of_emotions, testSet.examples);
+    predicted_targets_set = cat(2,predicted_targets_set,predictions); %adds a column for each example
+    actual_targets_set = cat(2,actual_targets_set,testSet.targets); %adds a column for the target's matrix
+    
+    confMatrixFold = generate_confusion_matrix(predictions, testSet.targets);
+    plot_confusion_matrix(confMatrixFold);
+    rp = calculate_recall_precision(confMatrixFold);
+    f_measures_per_fold{i} = calculate_f_measure(rp,1)    
+end
+
+end
+
--- Assignment 5/run_assignment5.m
+++ Assignment 5/run_assignment5.m
@@ -1,15 +1,16 @@
 oldFolder = cd ('Assignment 2')
 
 f_measures_for_trees = testAllData()
+f_measures_for_trees_fold = test10FoldData('clean')
 
 cd(oldFolder)
 oldFolder = cd ('Assignment3')
 
-f_measures_for_NN = testAllDataNN()
+%f_measures_for_NN = testAllDataNN()
 
 cd(oldFolder)
 oldFolder = cd ('Assignment 4')
 
-f_measures_for_CBR = testAllDataCBR()
+%f_measures_for_CBR = testAllDataCBR()
 
 cd(oldFolder)
