09088189e14e9987b1e36126d0457d42eb1ec0ab
Aadil Khan <aadil.khan53@gmail.com>
2012-03-15 14:33:04 +0000

    testing of algorithms

---
+++ Assignment 5/test_algorithms.m
@@ -0,0 +1,42 @@
+function [tTests, anovaTests_P, anovaTests_ANOVATAB, anovaTests_STATS, multiCompareTests] = test_algorithms(fMeasuresDT, fMeasuresANN, fMeasuresCBR)
+
+%% Run T-Tests
+alpha = 1;
+
+%6 emotions, 3 algorithms
+emotions = 6;
+algorithms = 3;
+tTests = zeros(emotions,algorithms);
+
+for emotion = 1:emotions
+  % DT vs ANN
+  tTests(emotion,1) = ttest2(fMeasuresTrees(emotion,:),fMeasuresANN(emotion),alpha);
+  % DT vs CBR
+  tTests(emotion,2) = ttest2(fMeasuresTrees(emotion,:),fMeasuresCBR(emotion),alpha);
+  % ANN vs CBR
+  tTests(emotion,3) = ttest2(fMeasuresANN(emotion,:),fMeasuresCBR(emotion),alpha);
+end
+
+
+%% Run ANOVA Tests and Multi Comparison Tests
+anovaTests_P = cell(1,emotions);
+anovaTests_ANOVATAB = cell(1,emotions);
+anovaTests_STATS = cell(1,emotions);
+
+multiCompareTests = cell(1,emotions);
+
+for emotion = 1:emotions
+  DT_Col = fMeasuresDT(emotion,:);
+  ANN_Col = fMeasuresANN(emotion,:);
+  CBR_Col = fMeasuresCBR(emotion,:);
+
+  [P, ANOVATAB, STATS] = anova1([DT_Col ANN_Col CBR_Col],{'DTs','ANNs','CBR'},'off');
+  anovaTests_P(emotion) = {P};
+  anovaTests_ANOVATAB(emotion) = {ANOVATAB};
+  anovaTests_STATS(emotion) = {STATS};
+
+  %% Carry out Multi Comparison Test here
+  C = multcompare(STATS,'alpha',alpha,'display','off');
+  multiCompareTests(emotion) = {C};
+
+end
